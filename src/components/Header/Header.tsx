99f47acf8f0519f88c21894b9706d73ddfb3c6cb
feefs <feefs@berkeley.edu>
2022-03-04 20:59:21 -0800

    add stylelint + change pretty-quick to lint-staged

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,9 +17,12 @@ export default function Header(): JSX.Element {
   const [user, ,] = useAuthState(auth);
   return (
     <header className="Header">
-      <button onClick={googleLogin}>Sign in!</button>
-      <button onClick={googleSignOut}>Sign out!</button>
-      {user ? user.email : 'Not signed in!'}
+      <div className="flex-container">
+        <span className="logo">Duo-cards</span>
+        <button onClick={googleLogin}>Sign in!</button>
+        <button onClick={googleSignOut}>Sign out!</button>
+        {user ? user.email : 'Not signed in!'}
+      </div>
     </header>
   );
 }
