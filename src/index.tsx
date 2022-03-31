30a6d6fa3d97db1a522784685c34d47f26bc9ecc
feefs <feefs@berkeley.edu>
2022-03-31 13:58:05 -0700

    upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 import App from './components/App';
 import reportWebVitals from './ts/reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter basename="/duo-cards">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
