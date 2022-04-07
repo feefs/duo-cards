b637ec5f8d48a4ea01ad66eda63048bed51c3307
feefs <feefs@berkeley.edu>
2022-04-06 17:54:54 -0700

    switch to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.scss';
 import App from './components/App';
@@ -8,9 +8,9 @@ import reportWebVitals from './ts/reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/duo-cards">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
