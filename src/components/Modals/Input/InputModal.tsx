3946e136e2c6e7db963a207b361d9c90f6cf38de
feefs <feefs@berkeley.edu>
2022-08-18 15:40:10 -0700

    clean up ConfirmModal and InputModal

--- src/components/Modals/Input/InputModal.tsx
+++ src/components/Modals/Input/InputModal.tsx
@@ -1,34 +1,28 @@
-import { User } from 'firebase/auth';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
 import { BaseModalProps, Modal } from '../Modal';
 import './InputModal.scss';
 
 type InputModalProps = BaseModalProps & {
-  user: User | null | undefined;
   initialText: string;
   placeholderText: string;
   submitText: (value: string) => Promise<void>;
 };
 
 export function InputModal(props: InputModalProps): JSX.Element {
-  const { open, onClose, user, initialText, placeholderText, submitText } = props;
+  const { open, onClose, initialText, placeholderText, submitText } = props;
 
   const [text, setText] = useState<string>(initialText);
   const [submitted, setSubmitted] = useState<boolean>(false);
-
-  const canSubmit = useCallback(() => {
-    return user && text && !submitted;
-  }, [user, text, submitted]);
 
   return (
     <Modal innerClassName="input-modal" {...{ open, onClose }}>
       <>
         <input className="text" value={text} placeholder={placeholderText} onChange={(e) => setText(e.target.value)} />
         <button
-          className={'submit' + (canSubmit() ? '' : ' disabled')}
+          className={'submit' + (text && !submitted ? '' : ' disabled')}
           onClick={async () => {
-            if (canSubmit()) {
+            if (text && !submitted) {
               setSubmitted(true);
               await submitText(text);
             }
