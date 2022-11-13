7b4577d9b7610f0f509f8ec27cab8e30b1df1a0e
feefs <feefs@berkeley.edu>
2022-11-13 11:11:07 -0800

    bump dependencies + change test to use MemoryRouter

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { act, render, screen } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
@@ -9,13 +8,12 @@ const queryClient = new QueryClient();
 
 test('renders learn react link', async () => {
   await act(async () => {
-    const history = createMemoryHistory();
     render(
-      <Router location={'/'} navigator={history}>
+      <MemoryRouter>
         <QueryClientProvider client={queryClient}>
           <App />
         </QueryClientProvider>
-      </Router>
+      </MemoryRouter>
     );
   });
   const linkElement = screen.getByText(/Duo-cards/i);
